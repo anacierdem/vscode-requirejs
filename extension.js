6f83c1de07e01465c9ee6e8be12d09e087469f9e
Ali Naci Erdem <anacierdem@gmail.com>
2017-02-28 14:39:07 +0300

    Removesd unnecessary comments.

--- extension.js
+++ extension.js
@@ -1,16 +1,8 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 var vscode = require('vscode');
 
 const JS_MODE = { scheme: 'file' };
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(context) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "require-js" is now active!');
 
     var referenceProvider  = function() {
         var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
