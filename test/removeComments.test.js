2ef082f31c48f51a89e9b74827e6b33867f1eb1b
Michael Withagen <michael.withagen@live.com>
2017-07-09 11:56:14 +0200

    mend

---
+++ test/removeComments.test.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+const { ReferenceProvider } = require('../extension');
+const referenceProvider = new ReferenceProvider();
+
+suite('removeComments', () => {
+    test('removeComments should remove comments from text', () => {
+        const input = `
+            define(['./module'], function (module) {
+                // require();
+                /* comment */
+                /**
+                 * comment
+                 */
+            })
+        `;
+        const forceWhiteSpace = '';
+        const expected = `
+            define(['./module'], function (module) {
+                ${forceWhiteSpace}
+                ${forceWhiteSpace}
+                ${forceWhiteSpace}
+            })
+        `;
+        
+        assert.equal(referenceProvider.removeComments(input), expected);
+    });
+});
