cd0566dbdbbae1365385dc3c7d45b1e4fec37b6a
Michael Withagen <michael.withagen@live.com>
2017-07-16 15:48:27 +0200

    normalize paths for ci

--- test/searchModule.test.js
+++ test/searchModule.test.js
@@ -1,4 +1,5 @@
 const vscode = require('vscode');
+const { normalize } = require('path');
 const rootPath = __dirname.replace('test', '').replace(/\\/g, '/');
 const vscodeStub = Object.assign(vscode, {
     workspace: {
@@ -18,14 +19,14 @@ suite('searchModule', () => {
     test('searchModule should resolve with path for moduleA.js', () => {
         return referenceProvider.searchModule('../testFiles/test1.js', 'moduleA', '', true)
             .then(result => {
-                assert.equal(result.uri._path, `/${rootPath}testFiles/moduleA.js`);
+                assert.equal(normalize(result.uri._path), normalize(`/${rootPath}testFiles/moduleA.js`));
             });
     });
 
     test('searchModule should find method foo in moduleA.js', () => {
         return referenceProvider.searchModule('../testFiles/test3.js', 'moduleA', 'foo', true)
             .then(result => {
-                assert.equal(result.uri._path, `/${rootPath}testFiles/moduleA.js`);
+                assert.equal(normalize(result.uri._path), normalize(`/${rootPath}testFiles/moduleA.js`));
                 assert.equal(result.range._start._line, 2);
                 assert.equal(result.range._start._character, 8);
             });
