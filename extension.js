6fdc9ce271a61af3a0dc5ea4dc40a3e005261759
Michael Withagen <michael.withagen@live.com>
2017-07-17 23:42:02 +0200

    Refactoring to get multiple defintion/require statements working for directly navigating to a module attribute.

--- extension.js
+++ extension.js
@@ -1,4 +1,4 @@
-const vscode = require('vscode');
+const { Location, Range, Uri, Position, workspace, commands, languages } = require('vscode');
 const path = require('path');
 
 class ReferenceProvider {
@@ -21,6 +21,23 @@ class ReferenceProvider {
         str = this.stripWhiteSpaceAndNewLines(str);
         const match = /(define|require).*{/i.exec(str);
         return match && match[0] || null;
+    }
+
+    /**
+     * Get partial code from require/define statement untill given line and character
+     * @param {VSCODE Document} document
+     * @param {Int} line to stop
+     * @param {Int} character to stop
+     * @return {String} substring for given document ending at given line and character and starting at last define or require
+     */
+    getRequireOrDefineCodeUntillCharacter(document, line, character) {
+        const textBeforeChar = document.getText(new Range(0, 0, line, character));
+        const textBeforeCharWithoutComments = this.removeComments(textBeforeChar);
+        const lastOccuranceDefine = textBeforeCharWithoutComments.toLowerCase().lastIndexOf('define');
+        const lastOccuranceRequire = textBeforeCharWithoutComments.toLowerCase().lastIndexOf('require');
+        const lastOccuranceRequireOrDefine = lastOccuranceDefine > -1 ? lastOccuranceDefine : lastOccuranceRequire;
+
+        return textBeforeCharWithoutComments.substr(lastOccuranceRequireOrDefine > -1 ? lastOccuranceRequireOrDefine : 0);
     }
 
     /**
@@ -66,6 +83,8 @@ class ReferenceProvider {
      */
     findConstructor(document, needle, haystack) {
         const test = new RegExp("(?:"+needle+"\\s*=\\s*(?:new)?\\s*)([^\\s(;]*)", "ig");
+        const fullText = document.getText();
+        const haystackOffset = fullText !== haystack ? fullText.indexOf(haystack) : 0;
         let searchResult;
         
         const references = [];
@@ -73,11 +92,11 @@ class ReferenceProvider {
         do {
             searchResult = test.exec(haystack);
             if (searchResult) {
-                const newPosition = document.positionAt(searchResult.index + searchResult[0].length);
+                const newPosition = document.positionAt(searchResult.index + searchResult[0].length + haystackOffset);
 
                 const range = document.getWordRangeAtPosition(newPosition);
                 if(range) {
-                    references.push(new vscode.Location(document.uri, range));
+                    references.push(new Location(document.uri, range));
                 }
             }
         } while (searchResult);
@@ -98,12 +117,12 @@ class ReferenceProvider {
         if (!!modulePath.match(/^\./i)) {
             newUriPath = path.resolve(currentFilePath.replace(/\\[^\\/]+$/, ''), modulePath);
         } else {
-            newUriPath = path.resolve(vscode.workspace.rootPath, vscode.workspace.getConfiguration("requireModuleSupport").get("modulePath"), modulePath);
+            newUriPath = path.resolve(workspace.rootPath, workspace.getConfiguration("requireModuleSupport").get("modulePath"), modulePath);
         }
         if (!newUriPath.match(/\.js$/i)) newUriPath += '.js';
 
-        const newUri = vscode.Uri.file(newUriPath);
-        const newDocument = vscode.workspace.openTextDocument(newUri);
+        const newUri = Uri.file(newUriPath);
+        const newDocument = workspace.openTextDocument(newUri);
 
         return new Promise(resolve => {
             newDocument.then(doc => {
@@ -112,7 +131,7 @@ class ReferenceProvider {
                 let searchResult;
                 let found = false;
 
-                const onlyNavigateToFile = vscode.workspace.getConfiguration("requireModuleSupport").get("onlyNavigateToFile");
+                const onlyNavigateToFile = workspace.getConfiguration("requireModuleSupport").get("onlyNavigateToFile");
 
                 if(!onlyNavigateToFile) {
                     do {
@@ -126,16 +145,16 @@ class ReferenceProvider {
                             const simpleComment = /^\s*\*/gm;
                             if(!simpleComment.test(doc.lineAt(newPosition._line).text)) {
                                 if(stopSearchingFurther) {
-                                    resolve( new vscode.Location(newUri, newPosition) );
+                                    resolve( new Location(newUri, newPosition) );
                                     return;
                                 } else {
                                     //Invoke a new providerbeginning from the new location
-                                    vscode.commands.executeCommand('vscode.executeDefinitionProvider', newUri, newPosition).then(refs => {
+                                    commands.executeCommand('vscode.executeDefinitionProvider', newUri, newPosition).then(refs => {
                                         if(refs.length > 0) {
                                             resolve( refs );
                                             return;
                                         } else {
-                                            resolve( new vscode.Location(newUri, newPosition) )
+                                            resolve( new Location(newUri, newPosition) )
                                             return;
                                         }
                                     });
@@ -148,7 +167,7 @@ class ReferenceProvider {
 
                 //Only navigate to the file
                 if(!found || onlyNavigateToFile) {
-                    resolve( new vscode.Location(newUri, new vscode.Position(0, 0) ));
+                    resolve( new Location(newUri, new Position(0, 0) ));
                     return;
                 }
             }, () => resolve(undefined));
@@ -174,9 +193,9 @@ class ReferenceProvider {
         while(char = line[range._start._character+endOffset], char != "'" && char != "\"" && range._start._character+endOffset < line.length) {
             endOffset++;
         }
-        return document.getText( new vscode.Range(
-                new vscode.Position(range._start._line, range._start._character-startOffset+1),
-                new vscode.Position(range._start._line, range._start._character+endOffset)
+        return document.getText( new Range(
+                new Position(range._start._line, range._start._character-startOffset+1),
+                new Position(range._start._line, range._start._character+endOffset)
             ))
     }
 
@@ -215,8 +234,8 @@ class ReferenceProvider {
 
         if(range) {
             const textAtCaret = document.getText(range);
-            const fullTextWithoutComments = this.removeComments(fullText);
-            const requireOrDefineStatement = this.getRequireOrDefineStatement(fullTextWithoutComments);
+            const codeBlockUntillTextAtCaret = this.getRequireOrDefineCodeUntillCharacter(document, range._start._line, range._end._character);
+            const requireOrDefineStatement = this.getRequireOrDefineStatement(codeBlockUntillTextAtCaret);
 
             if(requireOrDefineStatement) {
                 moduleList = this.getModulesWithPathFromRequireOrDefine(requireOrDefineStatement);
@@ -244,23 +263,27 @@ class ReferenceProvider {
                     let continueFrom;
 
                     let dotPosition = range._start._character >= 1 ?
-                                        document.offsetAt(new vscode.Position(range._start._line, range._start._character-1)) :
+                                        document.offsetAt(new Position(range._start._line, range._start._character-1)) :
                                         0;
 
                     //Do backwards search for a dot
-                    dotPosition = this.doBackwardsSearch(fullText, dotPosition, ".")
+                    dotPosition = this.doBackwardsSearch(fullText, dotPosition, ".");
                     const hasParent = dotPosition !== false;
 
                     let tmpModuleName;
                     if(!hasParent) {
-                        tmpModuleName = this.extractString(document, range)
+                        tmpModuleName = this.extractString(document, range);
                     }
 
-                    const constructors = this.findConstructor(document, textAtCaret, fullText);
+                    const constructors = this.findConstructor(document, textAtCaret, codeBlockUntillTextAtCaret);
                     //TODO: also consider window. defined globals
                     //Dont have a parent and have a constructor, follow the constructor
                     if(constructors.length && !hasParent) {
-                        let constructorName = document.getText(document.getWordRangeAtPosition(constructors[0].range._start));
+                        let constructorName = document.getText(
+                            document.getWordRangeAtPosition(
+                                new Position(constructors[0].range._start._line, constructors[0].range._start._character)
+                            )
+                        );
                         //Break search in case the instance and the constructor have the same name
                         if(constructorName === textAtCaret) {
                             resolve(undefined);
@@ -278,11 +301,11 @@ class ReferenceProvider {
                         let bracketPosition = document.offsetAt(propertyParentPosition);
 
                         //Do backwards search for a ")"
-                        bracketPosition = this.doBackwardsSearch(fullText, bracketPosition, ")")
+                        bracketPosition = this.doBackwardsSearch(fullText, bracketPosition, ")");
 
                         //Immediately invoked define/require
                         if(bracketPosition !== false) {
-                            const line = document.lineAt(propertyParentPosition._line).text
+                            const line = document.lineAt(propertyParentPosition._line).text;
                             const path = /['"]([^'"]*)/gi.exec(line);
 
                             if(path.length == 0) {
@@ -321,7 +344,7 @@ class ReferenceProvider {
 
                     //Should we continue searching? If so re-invoke a definition provider
                     if(continueFrom) {
-                        vscode.commands.executeCommand('vscode.executeDefinitionProvider', document.uri, continueFrom).then(refs => {
+                        commands.executeCommand('vscode.executeDefinitionProvider', document.uri, continueFrom).then(refs => {
 
                             for(let i = refs.length-1; i >= 0; i--) {
                                 //Discard if same file
@@ -349,7 +372,7 @@ Object.assign(exports, {
     ReferenceProvider,
     activate(context) {
         context.subscriptions.push(
-            vscode.languages.registerDefinitionProvider(
+            languages.registerDefinitionProvider(
                 { scheme: 'file' }, 
                 new ReferenceProvider()
             )
