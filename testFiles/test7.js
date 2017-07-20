44f443bdf156a8cdc3db613c247f990dc8f0da80
Ali Naci Erdem <anacierdem@gmail.com>
2017-07-20 13:50:00 +0300

    Edited test files

--- testFiles/test7.js
+++ testFiles/test7.js
@@ -1,7 +1,14 @@
-require(['moduleA', 
-            'moduleB'], function(a, b) {
+require(['moduleA', 'moduleB'], function(a, b) {
     var foo = a;
     var bar = b;
     foo.baz();
     bar.prop;
+});
+define('myName', ['moduleC', 'moduleD'], function(c, d) {
+    var foo = new c();
+    foo.bar();
+});
+require('moduleA').foo();
+define(function(require) {
+    var moduleA = require('moduleB');
 });
