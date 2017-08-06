08e189d478a558b0fd326d2224f730c27e9bf499
Michael Withagen <michael.withagen@live.com>
2017-08-06 14:19:08 +0200

    Introduce the good parts eslint + fix issues

--- test/extension.test.js
+++ test/extension.test.js
@@ -2,31 +2,26 @@ const sinon = require('sinon');
 const proxyquire = require('proxyquire');
 const assert = require('assert');
 const registerDefinitionProviderStub = sinon.stub();
-const vscodeStub = {
-    languages: { 
-        registerDefinitionProvider: registerDefinitionProviderStub
-    }
-};
+const vscodeStub = { languages: { registerDefinitionProvider: registerDefinitionProviderStub } };
 const extension = proxyquire('../extension', { vscode: vscodeStub });
 
 suite('extension', () => {
-    test('should export activate method', () => {
-        assert.ok('activate' in extension);
-    });
+  test('should export activate method', () => {
+    assert.ok('activate' in extension);
+  });
 
-    test('activate should register definition provider', () => {
-        const context = {
-            subscriptions: []
-        };
-        extension.activate(context);
+  test('activate should register definition provider', () => {
+    const context = { subscriptions: [] };
 
-        assert.equal(context.subscriptions.length, 1);
-        assert.deepEqual(
+    extension.activate(context);
+
+    assert.equal(context.subscriptions.length, 1);
+    assert.deepEqual(
             registerDefinitionProviderStub.getCall(0).args,
-            [
+      [
                 { scheme: 'file' },
-                new extension.ReferenceProvider()
-            ]
-        )
-    });
+        new extension.ReferenceProvider()
+      ]
+        );
+  });
 });
--- test/searchModule.test.js
+++ test/searchModule.test.js
@@ -2,33 +2,33 @@ const vscode = require('vscode');
 const { normalize } = require('path');
 const rootPath = __dirname.replace('test', '').replace(/\\/g, '/');
 const vscodeStub = Object.assign(vscode, {
-    workspace: {
-        rootPath,
-        openTextDocument: vscode.workspace.openTextDocument,
-        getConfiguration() { 
-            return { get: conf => conf === 'modulePath' ? 'testFiles' : false }
-        }
+  workspace: {
+    rootPath,
+    openTextDocument: vscode.workspace.openTextDocument,
+    getConfiguration () {
+      return { get: conf => conf === 'modulePath' ? 'testFiles' : false };
     }
+  }
 });
 const assert = require('assert');
 const proxyquire = require('proxyquire');
-const { ReferenceProvider } = proxyquire('../extension', { 'vscode': vscodeStub });
+const { ReferenceProvider } = proxyquire('../extension', { vscode: vscodeStub });
 const referenceProvider = new ReferenceProvider();
 
 suite('searchModule', () => {
-    test('should resolve with path for moduleA.js', () => {
-        return referenceProvider.searchModule('../testFiles/test1.js', 'moduleA', '', true)
+  test('should resolve with path for moduleA.js', () => {
+    return referenceProvider.searchModule('../testFiles/test1.js', 'moduleA', '', true)
             .then(result => {
-                assert.equal(normalize(result.uri._path), normalize(`/${rootPath}testFiles/moduleA.js`));
+              assert.equal(normalize(result.uri._path), normalize(`/${rootPath}testFiles/moduleA.js`));
             });
-    });
+  });
 
-    test('should find method foo in moduleA.js', () => {
-        return referenceProvider.searchModule('../testFiles/test3.js', 'moduleA', 'foo', true)
+  test('should find method foo in moduleA.js', () => {
+    return referenceProvider.searchModule('../testFiles/test3.js', 'moduleA', 'foo', true)
             .then(result => {
-                assert.equal(normalize(result.uri._path), normalize(`/${rootPath}testFiles/moduleA.js`));
-                assert.equal(result.range._start._line, 2);
-                assert.equal(result.range._start._character, 8);
+              assert.equal(normalize(result.uri._path), normalize(`/${rootPath}testFiles/moduleA.js`));
+              assert.equal(result.range._start._line, 2);
+              assert.equal(result.range._start._character, 8);
             });
-    });
+  });
 });
